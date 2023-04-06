f94d84b9beb15722ef373f51d2bda18b20485199
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:45:32 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MayBay mb = new MayBay();
+        mb.catCanh();
+        mb.haCanh();
+        mb.layVanToc();
+        
+        XeOto oto = new XeOto();
+        oto.layVanToc();
+        
+        XeDap xd = new XeDap();
+        xd.layVanToc();
+    }
+    
+}
