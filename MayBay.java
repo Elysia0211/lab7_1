441418feb717046f603cb9125ea7e2a2c0bab64f
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:46:53 +0700

    Create MayBay.java

---
+++ MayBay.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7;
+
+/**
+ *
+ * @author Admin
+ */
+public class MayBay extends PhuongTienDiChuyen{
+    private String loaiNhienLieu;
+    
+    @Override
+    public double layVanToc() {
+        return 0;
+    }
+    public void catCanh(){
+        
+    }
+    public void haCanh(){
+        
+    }
+}
