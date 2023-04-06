fb63b8e54fd4a99bf68170776f04f413a436cc30
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:47:41 +0700

    Create PhuongTienDiChuyen.java

---
+++ PhuongTienDiChuyen.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7;
+
+/**
+ *
+ * @author Admin
+ */
+abstract class PhuongTienDiChuyen {
+    private String loaiPhuongTien;
+    private HangSanXuat hangSanXuat;
+    
+    abstract double layVanToc();
+    
+    public String layTenHangSanXuat(){
+        
+        return null;
+        
+    }
+    public void batDau(){
+        
+    }
+    public void tangToc(){
+        
+    }
+    public void dungLai(){
+        
+    }
+    
+}
