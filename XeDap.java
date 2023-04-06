30a33e490f003175dc9676aed67c7434697417ca
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:48:23 +0700

    Create XeDap.java

---
+++ XeDap.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7;
+
+/**
+ *
+ * @author Admin
+ */
+public class XeDap extends PhuongTienDiChuyen{
+
+    @Override
+    double layVanToc() {
+        return 0;
+    }
+    
+}
