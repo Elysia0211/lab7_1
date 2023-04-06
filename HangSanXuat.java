7641ec2e94e3ed7e16aef1b02a97b4a618252fd6
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-04-06 08:46:18 +0700

    Create HangSanXuat.java

---
+++ HangSanXuat.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7;
+
+/**
+ *
+ * @author Admin
+ */
+public class HangSanXuat {
+    private String tenHangSanXuat;
+    private String tenQuocGia;
+}
